a225c6f3f45531eddf596e331c373da48c23906d
Cagatay Civici <cagatay.civici@gmail.com>
2021-03-19 16:43:12 +0300

    Added FAQ block

--- src/app/components/marketing/faq/faq.module.ts
+++ src/app/components/marketing/faq/faq.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaqComponent } from './faq.component';
 import { FaqRoutingModule } from './faq-routing.module';
+import { AccordionModule } from 'primeng/accordion';
 import { BlockViewerModule } from '../../blockviewer/blockviewer.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FaqRoutingModule,
+    AccordionModule,
     BlockViewerModule
   ],
   declarations: [
