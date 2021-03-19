51539eb1bfa397c9708eb138ebe2663391725a50
Cagatay Civici <cagatay.civici@gmail.com>
2021-03-19 15:04:58 +0300

    Initial blocks

--- src/app/components/marketing/hero/hero.module.ts
+++ src/app/components/marketing/hero/hero.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeroComponent} from './hero.component';
 import {HeroRoutingModule} from './hero-routing.module';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   imports: [
     CommonModule,
-    HeroRoutingModule
+    HeroRoutingModule,
+    ButtonModule
   ],
   declarations: [
     HeroComponent
