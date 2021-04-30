88205d5a0e3cd0ced2848425b422b18bf8486db9
Cagatay Civici <cagatay.civici@gmail.com>
2021-04-30 14:18:48 +0300

    Implemented 3 more feature sections

--- src/app/components/marketing/feature/feature.module.ts
+++ src/app/components/marketing/feature/feature.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FeatureComponent } from './feature.component';
 import { FeatureRoutingModule } from './feature-routing.module';
 import { BlockViewerModule } from '../../blockviewer/blockviewer.module';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   imports: [
     CommonModule,
     FeatureRoutingModule,
-    BlockViewerModule
+    BlockViewerModule,
+    ButtonModule
   ],
   declarations: [
     FeatureComponent
