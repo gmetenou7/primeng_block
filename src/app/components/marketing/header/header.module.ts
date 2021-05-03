6f226a610872212351c538dd510a5dd89a18067b
Merve Özçifçi <merveozcifci7@gmail.com>
2021-05-03 13:33:55 +0300

    Header blocks added

--- src/app/components/marketing/header/header.module.ts
+++ src/app/components/marketing/header/header.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header.component';
-import { HeaderRoutingModule } from './header-routing.module';
-import { BlockViewerModule } from '../../blockviewer/blockviewer.module';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header.component';
+import {HeaderRoutingModule} from './header-routing.module';
+import {BlockViewerModule} from '../../blockviewer/blockviewer.module';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {CalendarModule} from 'primeng/calendar';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -12,10 +14,13 @@ import { InputTextModule } from 'primeng/inputtext';
     HeaderRoutingModule,
     ButtonModule,
     BlockViewerModule,
-    InputTextModule
+    InputTextModule,
+    CalendarModule,
+    FormsModule
   ],
   declarations: [
     HeaderComponent
   ]
 })
-export class HeaderModule {}
+export class HeaderModule {
+}
