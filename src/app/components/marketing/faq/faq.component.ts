a7772fd0c8ad5fa141b2d71aa5d89e2471c2c5dc
Cagatay Civici <cagatay.civici@gmail.com>
2021-05-03 16:04:30 +0300

    Finished FAQ section

--- src/app/components/marketing/faq/faq.component.ts
+++ src/app/components/marketing/faq/faq.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
-  templateUrl: './faq.component.html'
+    templateUrl: './faq.component.html'
 })
 export class FaqComponent {
 
+    block1: string = ``;
+
+    block2: string = ``;
 }
