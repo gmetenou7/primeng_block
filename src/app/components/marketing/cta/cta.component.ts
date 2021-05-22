c18e7748798e9d7d5e6c0d1b0f93236436238d22
Cagatay Civici <cagatay.civici@gmail.com>
2021-05-22 13:19:48 +0300

    Moved headers to cta

--- src/app/components/marketing/cta/cta.component.ts
+++ src/app/components/marketing/cta/cta.component.ts
@@ -11,4 +11,16 @@ export class CtaComponent {
     block3: string = ``;
 
     block4: string = ``;
+    
+    block5: string = ``;
+
+    block6: string = ``;
+
+    block7: string = ``;
+
+    block8: string = ``;
+
+    date1 = '31/03/2021 - 01/04/2021';
+
+    date2 = '31/03/2021 - 01/04/2021';
 }
--- src/app/components/marketing/cta/cta.module.ts
+++ src/app/components/marketing/cta/cta.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CtaComponent } from './cta.component';
 import { CtaRoutingModule } from './cta-routing.module';
-import { ButtonModule} from 'primeng/button';
 import { BlockViewerModule } from '../../blockviewer/blockviewer.module';
+import { ButtonModule} from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     CtaRoutingModule,
     ButtonModule,
+    InputTextModule,
+    CalendarModule,
+    FormsModule,
     BlockViewerModule
   ],
   declarations: [
