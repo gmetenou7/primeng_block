3bbee0e2cd4431c34cb919819bf1a64f0647fbbe
Cagatay Civici <cagatay.civici@gmail.com>
2021-05-27 17:20:51 +0300

    Implemented Section Headings

--- src/app/components/application/sectionheading/sectionheading.component.ts
+++ src/app/components/application/sectionheading/sectionheading.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  templateUrl: './sectionheading.component.html'
+    templateUrl: './sectionheading.component.html'
 })
-export class SectionHeadingComponent {
+export class SectionHeadingComponent implements OnInit {
 
-  block1 = ``;
+    block1 = ``;
 
-  block2 = ``;
+    block2 = ``;
 
-  block3 = ``;
+    block3 = ``;
 
-  block4 = ``;
+    block4 = ``;
 
+    items: MenuItem[];
+
+    ngOnInit() {
+        this.items = [
+            {label: 'Overview', icon: 'pi pi-home'},
+            {label: 'Members', icon: 'pi pi-users'},
+            {label: 'Sales', icon: 'pi pi-shopping-cart'},
+            {label: 'Profile', icon: 'pi pi-user'},
+            {label: 'Settings', icon: 'pi pi-cog'}
+        ];
+    }
+    
 }
