f3d09a5978ec11a2ee809f1f5e9a2096725c6277
Cagatay Civici <cagatay.civici@gmail.com>
2021-06-04 14:39:54 +0300

    Implemented notifications

--- src/app/components/application/notification/notification.component.ts
+++ src/app/components/application/notification/notification.component.ts
@@ -13,22 +13,34 @@ export class NotificationComponent  {
     block2 = ``;
 
     notify1() {
+        this.messageService.clear('block2');
         this.messageService.add({
             key: 'block1',
-            severity: 'inxfo',
+            severity: 'custom-1',
             summary: 'Message Title',
             detail: 'Sagittis eu volutpat odio facilisis mauris sit amet. Sed velit dignissim sodales ut eu sem integer.',
-            sticky: true
+            sticky: true,
+            closable: false,
+            styleClass: 'surface-0',
+            contentStyleClass: 'p-0'
         });
     }
 
     notify2() {
+        this.messageService.clear('block1');
         this.messageService.add({
             key: 'block2',
-            severity: 'info',
+            severity: 'custom-2',
             summary: 'Message Title',
-            detail: 'Sagittis eu volutpat odio facilisis mauris sit amet. Sed velit dignissim sodales ut eu sem integer.'
+            closable: false,
+            detail: 'Sagittis eu volutpat odio facilisis mauris sit amet. Sed velit dignissim sodales ut eu sem integer.',
+            styleClass: 'surface-900',
+            contentStyleClass: 'p-0'
         });
+    }
+
+    clear(key: string) {
+        this.messageService.clear(key);
     }
 
 }
