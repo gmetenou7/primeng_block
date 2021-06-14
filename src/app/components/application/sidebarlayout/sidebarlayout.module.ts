8392bab5cc23d4c85187d355a5c18d3bd9d4daa5
Cagatay Civici <cagatay.civici@gmail.com>
2021-06-14 12:02:36 +0300

    First sidebar layout

--- src/app/components/application/sidebarlayout/sidebarlayout.module.ts
+++ src/app/components/application/sidebarlayout/sidebarlayout.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarLayoutComponent } from './sidebarlayout.component';
 import { SidebarLayoutRoutingModule } from './sidebarlayout-routing.module';
+import { InputTextModule } from 'primeng/inputtext';
+import { StyleClassModule } from '../../../styleClass';
 import { BlockViewerModule } from '../../blockviewer/blockviewer.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    InputTextModule,
+    StyleClassModule,
     SidebarLayoutRoutingModule,
     BlockViewerModule
   ],
