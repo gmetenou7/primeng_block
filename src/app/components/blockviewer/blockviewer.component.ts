4b665ac88c6af7a0a3f56d20f7c7e66714d3b291
Cagatay Civici <cagatay.civici@gmail.com>
2021-06-23 16:04:41 +0300

    Add production lock

--- src/app/components/blockviewer/blockviewer.component.ts
+++ src/app/components/blockviewer/blockviewer.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 enum BlockView {
     PREVIEW,
@@ -12,9 +13,12 @@ enum BlockView {
             <div class="block-header">
                 <span class="block-title">{{header}}</span>
                 <div class="block-actions">
-                    <a tabindex="0" [ngClass]="{'block-action-active': blockView == BlockView.PREVIEW}" (click)="activateView($event, BlockView.PREVIEW)">Preview</a>
-                    <a tabindex="0" [ngClass]="{'block-action-active': blockView == BlockView.CODE}" (click)="activateView($event, BlockView.CODE)">Code</a>
-                    <a tabindex="0" class="block-action-copy" (click)="copyCode($event)"><i class="pi pi-copy"></i></a>
+                    <a tabindex="0" [ngClass]="{'block-action-active': blockView == BlockView.PREVIEW}" (click)="activateView($event, BlockView.PREVIEW)"><span>Preview</span></a>
+                    <a tabindex="0" [ngClass]="{'block-action-active': blockView == BlockView.CODE, 'block-action-disabled': codeDisabled}" (click)="activateView($event, BlockView.CODE)">
+                        <i class="pi pi-lock" *ngIf="codeDisabled"></i>
+                        <span>Code</span>
+                    </a>
+                    <a tabindex="0" class="block-action-copy" [ngClass]="{'block-action-disabled': codeDisabled}" (click)="copyCode($event)"><i class="pi pi-copy"></i></a>
                 </div>
             </div>
             <div class="block-content">
@@ -22,7 +26,7 @@ enum BlockView {
                     <ng-content></ng-content>   
                 </div>
                 <div class="block-code" [ngClass]="{'block-content-active': blockView == BlockView.CODE}">
-<pre class="language-markup"><code #codeblock>{{code}}
+<pre class="language-markup" *ngIf="!codeDisabled"><code #codeblock>{{code}}
 
 </code></pre>
                 </div>
@@ -40,6 +44,8 @@ export class BlockViewer implements AfterViewInit {
 
     @Input() previewStyle: string;
 
+    @Input() free: boolean = false;
+
     @ViewChild('codeblock') codeViewChild: ElementRef;
 
     BlockView = BlockView;
@@ -47,12 +53,18 @@ export class BlockViewer implements AfterViewInit {
     blockView: BlockView = BlockView.PREVIEW;
 
     activateView(event: Event, blockView: BlockView) {
-        this.blockView = blockView;
+        if (!this.codeDisabled) {
+            this.blockView = blockView;
+        }
+        
         event.preventDefault();
     }
 
     async copyCode(event: Event) {
-        await navigator.clipboard.writeText(this.code);
+        if (!this.codeDisabled) {
+            await navigator.clipboard.writeText(this.code);
+        }
+
         event.preventDefault();
     }
 
@@ -60,5 +72,9 @@ export class BlockViewer implements AfterViewInit {
         if (window['Prism']) {
             window['Prism'].highlightElement(this.codeViewChild.nativeElement);
         }
+    }
+
+    get codeDisabled() {
+        return this.free ? false : (environment ? environment.production: false);
     }
 }
