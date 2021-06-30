8d621e5850c47c700ca61b8d4e4a1231e156b368
Cagatay Civici <cagatay.civici@gmail.com>
2021-06-30 11:19:19 +0300

    Implemented pricing page

--- src/app/components/pricing/pricing.module.ts
+++ src/app/components/pricing/pricing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 import { PricingComponent } from './pricing.component';
 import { PricingRoutingModule } from './pricing-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    ButtonModule,
     PricingRoutingModule
   ],
   declarations: [
