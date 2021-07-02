5a9eabcd311fdbf4eb7cf759a4a3c72ce7adb4a4
Cagatay Civici <cagatay.civici@gmail.com>
2021-07-02 18:15:19 +0300

    Color fixes to app navbars and stacked layout

--- src/app/components/application/stackedlayout/stackedlayout.module.ts
+++ src/app/components/application/stackedlayout/stackedlayout.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StackedLayoutComponent } from './stackedlayout.component';
 import { StyleClassModule } from '../../../styleClass';
+import { InputTextModule } from 'primeng/inputtext';
 import { StackedLayoutRoutingModule } from './stackedlayout-routing.module';
 import { BlockViewerModule } from '../../blockviewer/blockviewer.module';
 
@@ -10,6 +11,7 @@ import { BlockViewerModule } from '../../blockviewer/blockviewer.module';
     CommonModule,
     StackedLayoutRoutingModule,
     StyleClassModule,
+    InputTextModule,
     BlockViewerModule
   ],
   declarations: [
