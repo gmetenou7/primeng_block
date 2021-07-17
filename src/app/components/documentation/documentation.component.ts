6c04474fd71162d629866f6a3dfe7af036a6c24a
Cagatay Civici <cagatay.civici@gmail.com>
2021-07-17 17:13:58 +0300

    Docs

--- src/app/components/documentation/documentation.component.ts
+++ src/app/components/documentation/documentation.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './documentation.component.html'
+    templateUrl: './documentation.component.html',
+    styles: [`
+        :host ::ng-deep i {
+        background-color: var(--surface-a);
+        color: #2196f3;
+        font-family: Monaco,courier,monospace;
+        font-style: normal;
+        font-size: 12px;
+        font-weight: 500;
+        padding: 2px 4px;
+        letter-spacing: .5px;
+        border-radius: 3px;
+        font-weight: 600;
+        margin: 0 2px;
+        }
+  `]
 })
 export class DocumentationComponent {
 
--- src/app/components/documentation/documentation.module.ts
+++ src/app/components/documentation/documentation.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocumentationComponent } from './documentation.component';
 import { DocumentationRoutingModule } from './documentation-routing.module';
+import { AppCodeModule } from '../../app.code.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    AppCodeModule,
     DocumentationRoutingModule
   ],
   declarations: [
