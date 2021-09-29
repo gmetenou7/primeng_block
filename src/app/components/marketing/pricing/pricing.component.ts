a55116fffc1a067d8eb2c801643682a975a84c97
Cagatay Civici <cagatay.civici@gmail.com>
2021-09-29 13:39:05 +0300

    New pricing block

--- src/app/components/marketing/pricing/pricing.component.ts
+++ src/app/components/marketing/pricing/pricing.component.ts
@@ -231,4 +231,85 @@ export class PricingComponent {
     </div>
 </div>`;
 
+block3: string = `
+<div class="surface-section px-4 py-8 md:px-6 lg:px-8 border-round">
+    <div class="overflow-x-auto">
+        <table class="w-full white-space-nowrap" style="border-collapse: collapse; border-spacing: 0">
+            <thead>
+                <tr>
+                    <td class="surface-100 p-3">
+                        <div class="text-3xl text-900 mb-2">Pricing</div>
+                        <span class="text-600 line-height-3">Fringilla est ullamcorper eget nulla facilisi etiam</span>
+                    </td>
+                    <td class="surface-100 p-3 text-center">
+                        <div class="text-900 text-xl font-medium mb-3">Basic</div>
+                        <div class="text-600 mb-3"><span class="text-3xl text-900">$10</span> /month</div>
+                        <button pButton type="button" class="p-button-rounded" label="Free Trial"></button>
+                    </td>
+                    <td class="surface-100 p-3 text-center">
+                        <div class="text-900 text-xl font-medium mb-3">Premium</div>
+                        <div class="text-600 mb-3"><span class="text-3xl text-900">$10</span> /month</div>
+                        <button pButton type="button" class="p-button-rounded" label="Buy Now"></button>
+                    </td>
+                    <td class="surface-100 p-3 text-center">
+                        <div class="text-900 text-xl font-medium mb-3">Premium</div>
+                        <div class="text-600 mb-3"><span class="text-3xl text-900">$30</span> /month</div>
+                        <button pButton type="button" class="p-button-rounded p-button-outlined" label="Contact Us"></button>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="p-3 text-900 font-medium border-bottom-1 border-200">Arcu purus</td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                </tr>
+                <tr>
+                    <td class="p-3 text-900 font-medium border-bottom-1 border-200">Posuere felis</td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                </tr>
+                <tr>
+                    <td class="p-3 text-900 font-medium border-bottom-1 border-200">Curabitur mollis</td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                </tr>
+                <tr>
+                    <td class="p-3 text-900 font-medium border-bottom-1 border-200">Vitae odio</td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                </tr>
+                <tr>
+                    <td class="p-3 text-900 font-medium border-bottom-1 border-200">Orci, mi</td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-500 pi pi-times"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                </tr>
+                <tr>
+                    <td class="p-3 text-900 font-medium border-bottom-1 border-200">Tincidunt vulputate</td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-500 pi pi-times"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-500 pi pi-times"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                </tr>
+                <tr>
+                    <td class="p-3 text-900 font-medium border-bottom-1 border-200">Quis felis</td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-500 pi pi-times"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-500 pi pi-times"></i></td>
+                    <td class="text-center p-medium border-bottom-1 border-200"><i class="text-green-500 pi pi-check"></i></td>
+                </tr>
+                <tr>
+                    <td class="p-3 text-900 font-medium border-bottom-1 border-200">Dignissim vivamus</td>
+                    <td class="text-center text-600">Up to <span class="text-900">5</span> files</td>
+                    <td class="text-center text-600">Up to <span class="text-900">10</span> files</td>
+                    <td class="text-center text-900">Unlimited</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>`;
+
 }
