522b42fadb82b24ed490ad0d2174d7d1648b5965
Cagatay Civici <cagatay.civici@gmail.com>
2021-10-04 11:46:23 +0300

    Added 8 new blocks for PageHeading

--- src/app/components/application/pageheading/pageheading.module.ts
+++ src/app/components/application/pageheading/pageheading.module.ts
@@ -4,6 +4,11 @@ import { PageHeadingComponent } from './pageheading.component';
 import { PageHeadingRoutingModule } from './pageheading-routing.module';
 import { BlockViewerModule } from '../../blockviewer/blockviewer.module';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { TagModule } from 'primeng/tag';
+import { MenuModule } from 'primeng/menu';
 import { RippleModule } from 'primeng/ripple';
 
 @NgModule({
@@ -11,6 +16,11 @@ import { RippleModule } from 'primeng/ripple';
     CommonModule,
     PageHeadingRoutingModule,
     ButtonModule,
+    InputTextModule,
+    MenuModule,
+    AvatarGroupModule,
+    AvatarModule,
+    TagModule,
     RippleModule,
     BlockViewerModule
   ],
