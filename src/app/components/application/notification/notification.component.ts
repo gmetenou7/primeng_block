75ce3b5184e6c82f5c2bfd22d388dea411281e1c
Cagatay Civici <cagatay.civici@gmail.com>
2021-10-08 17:01:24 +0300

    New toast blocks

--- src/app/components/application/notification/notification.component.ts
+++ src/app/components/application/notification/notification.component.ts
@@ -55,6 +55,47 @@ export class NotificationComponent  {
     </ng-template>
 </p-toast>`;
 
+    block3 = `
+<button pButton pRipple label="Display" (click)="notify3()"></button>
+<p-toast key="block3" [style]="{width: '30rem'}" [preventOpenDuplicates]="true">
+    <ng-template let-message pTemplate="message">
+        <div class="flex align-items-start flex-1">
+            <i class="pi pi-envelope text-cyan-900 text-2xl mr-3"></i>
+            <div>
+                <span class="text-xl font-medium text-cyan-900">{{message.summary}}</span>
+                <p class="text-cyan-800 mt-3">{{message.detail}}</p>
+            </div>
+        </div>
+    </ng-template>
+</p-toast>`;
+
+    block4 = `
+<button pButton pRipple label="Display" (click)="notify4()"></button>
+<p-toast key="block4" [style]="{width: '30rem'}" [preventOpenDuplicates]="true">
+    <ng-template let-message pTemplate="message">
+        <div class="flex align-items-center justify-content-between flex-1 align-self-center mr-3">
+            <span class="text-xl font-medium text-orange-900">{{message.detail}}</span>
+            <a class="cursor-pointer font-medium text-orange-700">Reply</a>
+        </div>
+    </ng-template>
+</p-toast>`;
+
+    block5 = `
+<button pButton pRipple label="Display" (click)="notify5()"></button>
+<p-toast key="block5" [style]="{width: '30rem'}" [preventOpenDuplicates]="true">
+    <ng-template let-message pTemplate="message">
+        <div class="flex align-items-start flex-1 align-self-center mr-3">
+            <img src="assets/images/blocks/avatars/circle-big/avatar-f-2.png" style="width:56px;height:56px" />
+            <div class="ml-3">
+                <span class="text-xl font-medium text-900">{{message.summary}}</span>
+                <p class="text-600 mt-2 mb-3">{{message.detail}}</p>
+                <button type="button" pButton class="p-button-sm mr-2" label="Reply" (click)="clear('block5')"></button>
+                <button type="button" pButton class="p-button-sm p-button-text" label="Hide" (click)="clear('block5')"></button>
+            </div>
+        </div>
+    </ng-template>
+</p-toast>`;
+
     notify1() {
         this.messageService.clear('block2');
         this.messageService.add({
@@ -79,6 +120,41 @@ export class NotificationComponent  {
             detail: 'Sagittis eu volutpat odio facilisis mauris sit amet. Sed velit dignissim sodales ut eu sem integer.',
             styleClass: 'surface-900',
             contentStyleClass: 'p-0'
+        });
+    }
+
+    notify3() {
+        this.messageService.clear('block3');
+        this.messageService.add({
+            key: 'block3',
+            severity: 'custom-3',
+            summary: 'Custom Icon',
+            detail: 'Sagittis eu volutpat odio facilisis mauris sit amet.',
+            styleClass: 'bg-cyan-500',
+            contentStyleClass: 'p-3'
+        });
+    }
+
+    notify4() {
+        this.messageService.clear('block4');
+        this.messageService.add({
+            key: 'block4',
+            severity: 'custom-4',
+            detail: 'You received 1 new message.',
+            styleClass: 'bg-orange-300',
+            contentStyleClass: 'p-3'
+        });
+    }
+
+    notify5() {
+        this.messageService.clear('block5');
+        this.messageService.add({
+            key: 'block5',
+            severity: 'custom-5',
+            summary: 'Arlene McCoy',
+            detail: 'Tristique nulla aliquet enim tortor at auctor 😅... Urna nunc id.',
+            styleClass: 'surface-overlay',
+            contentStyleClass: 'p-4'
         });
     }
 
