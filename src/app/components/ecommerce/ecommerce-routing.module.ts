fb18b741357d916d1a60bfb63948f57915d6b3e7
Cagatay Civici <cagatay.civici@gmail.com>
2022-03-09 12:10:11 +0300

    Initiated product lists

--- src/app/components/ecommerce/ecommerce-routing.module.ts
+++ src/app/components/ecommerce/ecommerce-routing.module.ts
@@ -7,6 +7,7 @@ import { EcommerceComponent } from './ecommerce.component';
         RouterModule.forChild([
             {path: '', component: EcommerceComponent},
             {path: 'productoverview', loadChildren: () => import('./productoverview/productoverview.module').then(m => m.ProductOverviewModule), data: {name: 'Product Overview'}},
+            {path: 'productlist', loadChildren: () => import('./productlist/productlist.module').then(m => m.ProductListModule), data: {name: 'Product List'}},
         ])
     ],
     exports: [
