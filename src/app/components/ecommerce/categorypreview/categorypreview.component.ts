70bd7ed5dcb85337f90eaa8d44587fbcd649961a
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-03-09 13:11:18 +0300

    init categorypreview

---
+++ src/app/components/ecommerce/categorypreview/categorypreview.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-categorypreview',
+    templateUrl: './categorypreview.component.html',
+})
+export class CategoryPreviewComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    block1: string = `
+`;
+  
+    block2: string = `
+`;
+  
+    block3: string = `
+`;
+  
+    block4: string = `
+`;
+  
+    block5: string = `
+`;
+  
+    block6: string = `
+`;
+  
+    block7: string = `
+`;
+
+}
--- src/app/components/ecommerce/ecommerce-routing.module.ts
+++ src/app/components/ecommerce/ecommerce-routing.module.ts
@@ -7,6 +7,7 @@ import { EcommerceComponent } from './ecommerce.component';
         RouterModule.forChild([
             {path: '', component: EcommerceComponent},
             {path: 'productoverview', loadChildren: () => import('./productoverview/productoverview.module').then(m => m.ProductOverviewModule), data: {name: 'Product Overview'}},
+            {path: 'categorypreview', loadChildren: () => import('./categorypreview/categorypreview.module').then(m => m.CategoryPreviewModule), data: {name: 'Category Preview'}},
         ])
     ],
     exports: [
