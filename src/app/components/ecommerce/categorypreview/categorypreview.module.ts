63a9a2abd6484847218bb91cbf72ab4e3a516f37
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-03-10 13:08:28 +0300

    add category preview blocks

--- src/app/components/ecommerce/categorypreview/categorypreview.module.ts
+++ src/app/components/ecommerce/categorypreview/categorypreview.module.ts
@@ -5,13 +5,14 @@ import {CategoryPreviewRoutingModule} from './categorypreview-routing.module';
 import {BlockViewerModule} from '../../blockviewer/blockviewer.module';
 import {ButtonModule} from 'primeng/button';
 import {RippleModule} from 'primeng/ripple';
-
+import {CarouselModule} from 'primeng/carousel';
 @NgModule({
   imports: [
     CommonModule,
     BlockViewerModule,
     ButtonModule,
     RippleModule,
+    CarouselModule,
     CategoryPreviewRoutingModule
   ],
   declarations: [
