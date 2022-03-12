e04244621a6813d0952b2338f4a95ef295094bd9
Cagatay Civici <cagatay.civici@gmail.com>
2022-03-12 15:22:25 +0300

    Implemented product quickviews

--- src/app/components/ecommerce/productquickview/productquickview.component.ts
+++ src/app/components/ecommerce/productquickview/productquickview.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     templateUrl: './productquickview.component.html'
 })
-export class ProductQuickviewComponent {
+export class ProductQuickviewComponent implements OnInit {
 
     hovered: boolean;
 
@@ -12,6 +12,32 @@ export class ProductQuickviewComponent {
     color1: string = 'cyan';
 
     size1: string = 'M';
+
+    images: string[];
+
+    responsiveOptions:any[] = [{
+        breakpoint: '1024px',
+        numVisible: 5
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 3
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1
+    }];
+
+    ngOnInit(): void {
+        this.images = [
+            'product-quickview-2-1.png',
+            'product-quickview-2-2.png',
+            'product-quickview-2-3.png',
+            'product-quickview-2-4.png',
+            'product-quickview-2-5.png',
+            'product-quickview-2-6.png'
+        ];
+    }
 
     block1: string = `
 `;
