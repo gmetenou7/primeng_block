d3a037c32c99803fdf9e5f965581e8b2a09449bc
Cagatay Civici <cagatay.civici@gmail.com>
2022-03-15 12:14:32 +0300

    First order summary block

--- src/app/components/ecommerce/ecommerce-routing.module.ts
+++ src/app/components/ecommerce/ecommerce-routing.module.ts
@@ -14,7 +14,8 @@ import { EcommerceComponent } from './ecommerce.component';
             {path: 'productquickview', loadChildren: () => import('./productquickview/productquickview.module').then(m => m.ProductQuickviewModule), data: {name: 'Product Qucikview'}},
             {path: 'productfeature', loadChildren: () => import('./productfeature/productfeature.module').then(m => m.ProductFeatureModule), data: {name: 'Product Features'}},
             {path: 'promosection', loadChildren: () => import('./promosection/promosection.module').then(m => m.ProductSectionModule), data: {name: 'Promo Section'}},
-            {path: 'review', loadChildren: () => import('./review/review.module').then(m => m.ReviewModule), data: {name: 'Review'}}
+            {path: 'review', loadChildren: () => import('./review/review.module').then(m => m.ReviewModule), data: {name: 'Review'}},
+            {path: 'ordersummary', loadChildren: () => import('./ordersummary/ordersummary.module').then(m => m.OrderSummaryModule), data: {name: 'Order Summary'}}
         ])
     ],
     exports: [
