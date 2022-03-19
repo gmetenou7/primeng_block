1aa2c83b5f6890f83443f571614e219f649a6f38
Cagatay Civici <cagatay.civici@gmail.com>
2022-03-19 21:13:26 +0300

    Fixed links

--- src/app/components/ecommerce/ecommerce-routing.module.ts
+++ src/app/components/ecommerce/ecommerce-routing.module.ts
@@ -7,8 +7,8 @@ import { EcommerceComponent } from './ecommerce.component';
         RouterModule.forChild([
             {path: '', component: EcommerceComponent},
             {path: 'productoverview', loadChildren: () => import('./productoverview/productoverview.module').then(m => m.ProductOverviewModule), data: {name: 'Product Overview'}},
-            {path: 'categorypreview', loadChildren: () => import('./categorypreview/categorypreview.module').then(m => m.CategoryPreviewModule), data: {name: 'Category Preview'}},
             {path: 'productlist', loadChildren: () => import('./productlist/productlist.module').then(m => m.ProductListModule), data: {name: 'Product List'}},
+            {path: 'categorypreview', loadChildren: () => import('./categorypreview/categorypreview.module').then(m => m.CategoryPreviewModule), data: {name: 'Category Preview'}},
             {path: 'shoppingcart', loadChildren: () => import('./shoppingcart/shoppingcart.module').then(m => m.ShoppingCartModule), data: {name: 'Shopping Cart'}},
             {path: 'categoryfilter', loadChildren: () => import('./categoryfilter/categoryfilter.module').then(m => m.CategoryFilterModule), data: {name: 'Category Filter'}},
             {path: 'productquickview', loadChildren: () => import('./productquickview/productquickview.module').then(m => m.ProductQuickviewModule), data: {name: 'Product Qucikview'}},
