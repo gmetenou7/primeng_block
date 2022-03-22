e41157360b425b57146a24079de8b193b6e402e6
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-03-22 13:23:26 +0300

    add productpage block

--- src/app/components/ecommerce/productpage/productpage.module.ts
+++ src/app/components/ecommerce/productpage/productpage.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { StyleClassModule } from 'primeng/styleclass';
+import { BlockViewerModule } from '../../blockviewer/blockviewer.module';
+import { TabViewModule } from 'primeng/tabview';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DropdownModule } from 'primeng/dropdown';
+import { GalleriaModule } from 'primeng/galleria';
+import { ProgressBarModule } from 'primeng/progressbar';
 import { ProductPageComponent } from './productpage.component';
 import { ProductPageRoutingModule } from './productpage-routing.module';
 
@@ -7,7 +17,17 @@ import { ProductPageRoutingModule } from './productpage-routing.module';
 @NgModule({
   imports: [
     CommonModule,
-    ProductPageRoutingModule
+    ProductPageRoutingModule,
+    ButtonModule,
+    RippleModule,
+    StyleClassModule,
+    BlockViewerModule,
+    DropdownModule,
+    GalleriaModule,
+    TabViewModule,
+    InputNumberModule,
+    FormsModule,
+    ProgressBarModule,
   ],
   declarations: [
     ProductPageComponent
