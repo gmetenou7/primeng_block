40aae1806f79b09e5bae7ccc03db5fae518ab2ce
Kerem YILDAN <74204161+dcyaner@users.noreply.github.com>
2022-11-14 17:23:10 +0300

    Sync "Application / List Container" blocks #73

--- src/app/components/application/listcontainer/listcontainer.component.ts
+++ src/app/components/application/listcontainer/listcontainer.component.ts
@@ -7,42 +7,42 @@ export class ListContainerComponent {
 
     block1 = `
 <ul class="list-none p-0 m-0">
-    <li class="pb-4 border-bottom-1 border-300">
-        <div class="border-2 border-dashed border-300 border-round surface-0 p-4"></div>
+    <li class="pb-4 border-bottom-1 surface-border">
+        <div class="border-2 border-dashed surface-border border-round surface-0 p-4"></div>
     </li>
-    <li class="py-4 border-bottom-1 border-300">
-        <div class="border-2 border-dashed border-300 border-round surface-0 p-4"></div>
+    <li class="py-4 border-bottom-1 surface-border">
+        <div class="border-2 border-dashed surface-border border-round surface-0 p-4"></div>
     </li>
     <li class="pt-4">
-        <div class="border-2 border-dashed border-300 border-round surface-0 p-4"></div>
+        <div class="border-2 border-dashed surface-border border-round surface-0 p-4"></div>
     </li>
 </ul>`;
 
     block2 = `
-<div class="surface-0 p-4 shadow-2 border-round">
+<div class="surface-card p-4 shadow-2 border-round">
     <ul class="list-none p-0 m-0">
-        <li class="pb-4 border-bottom-1 border-300">
-            <div class="border-2 border-dashed border-300 border-round surface-0 p-4"></div>
+        <li class="pb-4 border-bottom-1 surface-border">
+            <div class="border-2 border-dashed surface-border border-round surface-0 p-4"></div>
         </li>
-        <li class="py-4 border-bottom-1 border-300">
-            <div class="border-2 border-dashed border-300 border-round surface-0 p-4"></div>
+        <li class="py-4 border-bottom-1 surface-border">
+            <div class="border-2 border-dashed surface-border border-round surface-0 p-4"></div>
         </li>
         <li class="pt-4">
-            <div class="border-2 border-dashed border-300 border-round surface-0 p-4"></div>
+            <div class="border-2 border-dashed surface-border border-round surface-0 p-4"></div>
         </li>
     </ul>
 </div>`;
 
     block3 = `
 <ul class="list-none p-0 m-0">
-    <li class="surface-0 p-4 mb-3 shadow-2 border-round">
-        <div class="border-2 border-dashed border-300 border-round surface-0 p-4"></div>
+    <li class="surface-card p-4 mb-3 shadow-2 border-round">
+        <div class="border-2 border-dashed surface-border border-round surface-0 p-4"></div>
     </li>
-    <li class="surface-0 p-4 mb-3 shadow-2 border-round">
-        <div class="border-2 border-dashed border-300 border-round surface-0 p-4"></div>
+    <li class="surface-card p-4 mb-3 shadow-2 border-round">
+        <div class="border-2 border-dashed surface-border border-round surface-0 p-4"></div>
     </li>
-    <li class="surface-0 p-4 shadow-2 border-round">
-        <div class="border-2 border-dashed border-300 border-round surface-0 p-4"></div>
+    <li class="surface-card p-4 shadow-2 border-round">
+        <div class="border-2 border-dashed surface-border border-round surface-0 p-4"></div>
     </li>
 </ul>`;
 
