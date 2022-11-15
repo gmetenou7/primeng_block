34af032a73a550bb98b2ad438b28bf5a5733651f
Onur Senture <woof.uyelik@gmail.com>
2022-11-15 11:42:22 +0300

    Sync "Application / Action Panel" blocks #58

--- src/app/components/application/actionpanel/actionpanel.component.ts
+++ src/app/components/application/actionpanel/actionpanel.component.ts
@@ -7,7 +7,7 @@ export class ActionPanelComponent {
 
     block1 = `
 <div class="surface-card border-round shadow-2 p-4">
-    <div class="text-900 font-medium mb-3 text-xl">Title Placeholder</div>
+    <div class="text-900 font-medium mb-2 text-xl">Title Placeholder</div>
     <p class="mt-0 mb-4 p-0 line-height-3">Nunc mi ipsum faucibus vitae aliquet nec. Lacus sed viverra tellus in hac habitasse platea dictumst.</p>
     <button pButton pRipple label="Save Changes"></button>
 </div>`;
@@ -15,7 +15,7 @@ export class ActionPanelComponent {
     block2 = `
 <div class="surface-card border-round shadow-2 p-4 flex flex-column lg:flex-row align-items-start">
     <div>
-        <div class="text-900 font-medium mb-3 text-xl">Title Placeholder</div>
+        <div class="text-900 font-medium mb-2 text-xl">Title Placeholder</div>
         <p class="mt-0 mb-4 lg:mb-0 p-0 line-height-3">Nunc mi ipsum faucibus vitae aliquet nec. Lacus sed viverra tellus in hac habitasse platea dictumst.</p>
     </div>
     <button pButton pRipple label="Save" icon="pi pi-check" class="ml-0 lg:ml-5"></button>
@@ -23,7 +23,7 @@ export class ActionPanelComponent {
 
     block3 = `
 <div class="surface-card border-round shadow-2 p-4">
-    <div class="text-900 font-medium mb-3 text-xl">Title Placeholder</div>
+    <div class="text-900 font-medium mb-2 text-xl">Title Placeholder</div>
     <p class="mt-0 mb-4 p-0 line-height-3">Nunc mi ipsum faucibus vitae aliquet nec. Lacus sed viverra tellus in hac habitasse platea dictumst.</p>
     <div class="flex mb-4 flex-column lg:flex-row">
         <div class="surface-50 p-3 flex-auto">
@@ -39,12 +39,12 @@ export class ActionPanelComponent {
             <span class="text-blue-600 font-medium text-xl">14:30</span>
         </div>
     </div>
-    <button pButton pRipple label="Save Changes"></button>
+    <button pButton pRipple icon="pi pi-check" label="Confirm Reservation"></button>
 </div>`;
 
     block4 = `
 <div class="surface-card border-round shadow-2 p-4">
-    <div class="text-900 font-medium mb-3 text-xl">Change Phone</div>
+    <div class="text-900 font-medium mb-2 text-xl">Change Phone</div>
     <p class="mt-0 mb-4 p-0 line-height-3">Nunc mi ipsum faucibus vitae aliquet nec.</p>
     <div class="flex">
         <input pInputText class="flex-auto mr-3">
@@ -55,7 +55,7 @@ export class ActionPanelComponent {
     block5 = `
 <div class="surface-card border-round shadow-2 p-4 text-center">
     <img src="assets/images/blocks/illustration/subscribe.svg" alt="Image" class="mx-auto block mb-4">
-    <div class="text-900 font-medium mb-3 text-xl">Title Placeholder</div>
+    <div class="text-900 font-medium mb-2 text-xl">Title Placeholder</div>
     <p class="mt-0 mb-4 p-0 line-height-3">Nunc mi ipsum faucibus vitae aliquet nec. Lacus sed viverra tellus in hac habitasse platea dictumst.</p>
     <button pButton pRipple label="Learn More" icon="pi pi-arrow-right" class="p-button-rounded" iconPos="right"></button>
 </div>`;
@@ -66,7 +66,7 @@ export class ActionPanelComponent {
         <img src="assets/images/blocks/illustration/live-collaboration.svg" alt="Image" class="mx-auto block mb-4 w-full">
     </div>
     <div class="py-3 px-5 flex flex-column align-items-start">
-        <div class="text-900 font-medium mb-3 text-xl">Title Placeholder</div>
+        <div class="text-900 font-medium mb-2 text-xl">Title Placeholder</div>
         <p class="mt-0 mb-4 p-0 line-height-3">Nunc mi ipsum faucibus vitae aliquet nec.</p>
         <button pButton pRipple label="Proceed" class="mt-auto"></button>
     </div>
