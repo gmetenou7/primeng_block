844cf91088f3e3686632ea93e0a443bd44779e80
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-12-22 17:21:57 +0300

    Fixed #109

--- src/app/components/marketing/stats/stats.component.ts
+++ src/app/components/marketing/stats/stats.component.ts
@@ -190,14 +190,14 @@ export class StatsComponent {
 
     block6: string = `
 <section class="relative surface-section">
-    <div class="text-center bg-no-repeat bg-cover bg-gray-900 px-6 py-8">
+    <div class="text-center bg-gray-900 px-6 py-8">
         <h2 class="text-blue-300 text-lg font-semibold mb-1 mt-0">Why Bastion</h2>
         <h3 class="text-white font-semibold text-4xl mt-0 mb-0 mt-0 line-height-3">Build secure onboarding and authentication</h3>
         <p class="text-gray-500 font-medium text-base text-xs mt-2 mb-4 line-height-3">Egestas congue quisque egestas diam in arcu. Sapien eget mi proin sed libero enim sed faucibus.</p>
     </div>
     <div class="px-4 py-5 md:px-6 lg:px-8">
         <div class="flex flex-column lg:flex-row lg:align-items-center lg:justify-content-between">
-            <div class="px-2 w-full py-6 md:px-4 lg:px-6 bg-cover relative" style="background: url(assets/images/blocks/stats/stats-1.png); background-position: center; margin-top:-2rem; top:-70px;">
+            <div class="px-2 w-full py-6 md:px-4 lg:px-6 bg-cover relative border-round" style="background: url(assets/images/blocks/stats/stats-1.png); background-position: center; margin-top:-2rem; top:-70px;">
                 <div class="flex flex-column lg:flex-row">
                     <article class="w-full p-2 mr-0 lg:mr-3 lg:mb-0 mb-3 text-center" style="background: rgba(255, 255, 255, 0.25);border-radius: 6px;">
                         <h4 class="text-white text-6xl line-height-3 font-medium mt-3 mb-0">1 min</h4>
